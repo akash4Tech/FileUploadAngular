4cc40f3a0da5f7f2a08901bdd4d2bae9a7c51c70
AkashPandian <akashpandian.pandian@finstein.ai>
2023-08-16 23:20:05 +0530

    select flile fuction  added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FileuploadComponent } from './fileupload/fileupload.component';
@@ -8,6 +8,7 @@ import { FileuploadComponent } from './fileupload/fileupload.component';
 // prime ng imports
 
 import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,9 @@ import { ButtonModule } from 'primeng/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/fileupload/fileupload.component.ts
+++ src/app/fileupload/fileupload.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-fileupload',
   templateUrl: './fileupload.component.html',
   styleUrls: ['./fileupload.component.scss']
 })
-export class FileuploadComponent {
-  
-  constructor() {}
+export class FileuploadComponent implements OnInit {
 
-    
+
+  fileuploadUrl:any
+  Formatdata:any
+  constructor(
+
+    private http:HttpClientModule
+
+  ) {}
+
+
+
+
+
+  ngOnInit(): void {}
+
+
+  file:any
+
+  selectfile(event:any){
+
+    console.log(event);
+   this.file=event.target.files[0]
+   console.log(this.file);
+       
+
+  }
+
+
+   
+  // fileupload(){
+
+  //   let formData = new formData 
+  //   formData.append('file', this.file);
+  // }
 
 
 }
