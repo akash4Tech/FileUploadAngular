813182fc0ba58c8415e1944a5b69c4ca0c9aa5c6
AkashPandian <akashpandian.pandian@finstein.ai>
2023-08-16 22:15:34 +0530

    FileUpload Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileuploadComponent } from './fileupload/fileupload.component';
+
+// prime ng imports
+
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileuploadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/fileupload/fileupload.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fileupload',
+  templateUrl: './fileupload.component.html',
+  styleUrls: ['./fileupload.component.scss']
+})
+export class FileuploadComponent {
+  
+  constructor() {}
+
+    
+
+
+}
